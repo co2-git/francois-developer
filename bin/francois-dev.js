ca0d05c5e4475de01f8944f5c435164004131846
francois <francoisrvespa@gmail.com>
2013-10-12 11:34:42 +0200

    commented out hire me

--- bin/francois-dev.js
+++ bin/francois-dev.js
@@ -1,17 +1,22 @@
 #!/usr/bin/env node
-var forever = require('forever'),
-  path = require('path'),
-  Path = path.dirname(__dirname),
-  child = new(forever.Monitor)(Path + '/ui/app.js', {
-    'silent': false,
-    'pidFile': Path + '/admin/app.pid',
-    'watch': true,
-    'watchDirectory': Path + '/ui',
-    'watchIgnoreDotFiles': true,
-    'watchIgnorePatterns': [],
-    'logFile': Path + '/admin/forever.log',
-    'outFile': Path + '/admin/forever.out',
-    'errFile': Path + '/admin/forever.err'
-  });
-child.start();
-forever.startServer(child);
+// var forever = require('forever'),
+//   path = require('path'),
+//   Path = path.dirname(__dirname),
+//   child = new(forever.Monitor)(Path + '/ui/app.js', {
+//     'silent': false,
+//     'pidFile': Path + '/admin/app.pid',
+//     'watch': true,
+//     'watchDirectory': Path + '/ui',
+//     'watchIgnoreDotFiles': true,
+//     'watchIgnorePatterns': [],
+//     'logFile': Path + '/admin/forever.log',
+//     'outFile': Path + '/admin/forever.out',
+//     'errFile': Path + '/admin/forever.err'
+//   });
+// child.start();
+// forever.startServer(child);
+
+
+
+
+// node node_modules/.bin/forever -w start ui/app.js
