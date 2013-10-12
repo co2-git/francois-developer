80bbd9d46ff601f8bba0d56d3e40b48d792696a2
francois <francoisrvespa@gmail.com>
2013-10-12 17:19:15 +0200

    ok bash script

--- bin/francois-dev.js
+++ bin/francois-dev.js
@@ -1,20 +1,64 @@
 #!/usr/bin/env node
-// var forever = require('forever'),
-//   path = require('path'),
-//   Path = path.dirname(__dirname),
-//   child = new(forever.Monitor)(Path + '/ui/app.js', {
-//     'silent': false,
-//     'pidFile': Path + '/admin/app.pid',
-//     'watch': true,
-//     'watchDirectory': Path + '/ui',
-//     'watchIgnoreDotFiles': true,
-//     'watchIgnorePatterns': [],
-//     'logFile': Path + '/admin/forever.log',
-//     'outFile': Path + '/admin/forever.out',
-//     'errFile': Path + '/admin/forever.err'
-//   });
-// child.start();
-// forever.startServer(child);
+
+var action = process.argv[2],
+  defaultAction = 'help';
+
+if ( ! action ) {
+  action = defaultAction;
+}
+
+var actions = {
+  help: function help () {
+    console.log('francois-developer');
+    console.log({ actions: Object.keys(actions) });
+  },
+  
+  start: function start () {
+    var forever = require('forever'),
+      path = require('path'),
+      Path = path.dirname(__dirname),
+      child = new(forever.Monitor)(Path + '/ui/app.js', {
+        'silent': false,
+        'pidFile': Path + '/admin/app.pid',
+        'watch': true,
+        'watchDirectory': Path,
+        'watchIgnoreDotFiles': true,
+        'watchIgnorePatterns': ['admin/*'],
+        'logFile': Path + '/admin/forever.log',
+        'outFile': Path + '/admin/forever.out',
+        'errFile': Path + '/admin/forever.err'
+      });
+    child.start();
+    forever.startServer(child);
+  },
+
+  status: function status () {
+    var forever = require('forever'),
+      appStatus = 'stopped',
+      path = require('path'),
+      Path = path.dirname(__dirname);
+    forever.list(null, function (error, list) {
+      if ( error ) {
+        throw error;
+      }
+      list.forEach(function (p) {
+        if ( p.command === Path + '/ui/apps.js' ) {
+          appStatus = 'running';
+        }
+      });
+      console.log(appStatus);
+    });
+  }
+};
+
+if ( typeof actions[action] !== 'function' ) {
+  console.log('Unknown action', action, 'Using default action', defaultAction);
+  action = defaultAction;
+}
+
+actions[action]();
+
+  
 
 
 
